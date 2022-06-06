bc3fbb321b4740782d2ef08c5bea2bc42d16608a
ricardoriballo <ricardoriballo.dev@gmail.com>
2022-06-06 18:03:25 +0200

    Vue Clicker

---
+++ vuetest/vue-project/src/components/Clicker.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="addClick">Añadir Click</button>
+    <h2>{{ clicker }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Counter",
+      clicker: 0,
+    };
+  },
+  methods: {
+    addClick() {
+      this.clicker++;
+    },
+  },
+};
+</script>
+
+<style>
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
